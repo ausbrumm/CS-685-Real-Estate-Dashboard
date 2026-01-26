3653f8f82796d7afd5e8ac277b28c423b3235c08
ausbrumm <austin.brummett@outlook.com>
2026-01-26 12:49:56 -0600

    Updated db script

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -1,12 +1,18 @@
 CREATE DATABASE real_estate_db;
 
-CREATE TABLE IF NOT EXISTS metro_us (
-    id SERIAL PRIMARY KEY,
-    region_id NUMERIC(15) NOT NULL,
-    size_rank NUMERIC(15) NOT NULL,
-    region_name VARCHAR(255) NOT NULL,
-    state_name VARCHAR(255) NOT NULL,
-    date DATE NOT NULL,
-    avg_cost NUMERIC(15, 11),
-    source VARCHAR(100)
-);
+-- Table: public.metro_us
+
+-- DROP TABLE IF EXISTS public.metro_us;
+
+CREATE TABLE IF NOT EXISTS public.metro_us
+(
+    id integer NOT NULL DEFAULT nextval('metro_us_id_seq'::regclass),
+    region_id numeric(15,0) NOT NULL,
+    size_rank numeric(15,0) NOT NULL,
+    region_name character varying(255) COLLATE pg_catalog."default",
+    state_name character varying(255) COLLATE pg_catalog."default",
+    date date NOT NULL,
+    avg_cost numeric(15,2),
+    source character varying(100) COLLATE pg_catalog."default",
+    CONSTRAINT metro_us_pkey PRIMARY KEY (id)
+)
