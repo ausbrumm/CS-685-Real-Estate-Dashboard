c746e4c413e1c2564fe850953205d600453308ef
ausbrumm <austin.brummett@outlook.com>
2026-01-27 09:32:13 -0600

    Update to db script

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -6,13 +6,12 @@ CREATE DATABASE real_estate_db;
 
 CREATE TABLE IF NOT EXISTS public.metro_us
 (
-    id integer NOT NULL DEFAULT nextval('metro_us_id_seq'::regclass),
-    region_id numeric(15,0) NOT NULL,
-    size_rank numeric(15,0) NOT NULL,
-    region_name character varying(255) COLLATE pg_catalog."default",
-    state_name character varying(255) COLLATE pg_catalog."default",
+    id BIGINT NOT NULL DEFAULT nextval('metro_us_id_seq'::regclass),
+    region_id BIGINT NOT NULL,
+    size_rank INTEGER NOT NULL,
+    region_name TEXT COLLATE pg_catalog."default",
+    state_name TEXT COLLATE pg_catalog."default",
     date date NOT NULL,
     avg_cost numeric(15,2),
-    source character varying(100) COLLATE pg_catalog."default",
     CONSTRAINT metro_us_pkey PRIMARY KEY (id)
 )
